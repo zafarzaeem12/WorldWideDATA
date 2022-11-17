04a9daf5a1a677c21664c5b58a9a593a493e88b4
Zaeem Uz Zafar <zaeem.zafar@technado.pk>
2022-11-18 00:53:25 +0500

    changes with token formed

--- index.js
+++ index.js
@@ -22,18 +22,19 @@ const TokenData = async (country) => {
 // Getting Country Data Function start here
 
 const DataByCountry = async () => {
-    const token = await TokenData('HKRUQbh-ZAJw5gB6TGD9hhqBfjvfna2S5xgQRd25BB54j8lCYLIw0X7pWMbvU4M-3_0');
-    const bearer = JSON.stringify(`${token?.auth_token}`)
+    const access_token = 'HKRUQbh-ZAJw5gB6TGD9hhqBfjvfna2S5xgQRd25BB54j8lCYLIw0X7pWMbvU4M-3_0';
+    const token = await TokenData(access_token);
+    const bearer = token?.auth_token;
     const fetch_country = await fetch(`https://www.universal-tutorial.com/api/countries/`, {
         headers: {
             "Accept": "application/json",
-            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJ6YWVlbS56YWZhckBiaW5hdGVkaWdpdGFsLmNvbSIsImFwaV90b2tlbiI6IkhLUlVRYmgtWkFKdzVnQjZUR0Q5aGhxQmZqdmZuYTJTNXhnUVJkMjVCQjU0ajhsQ1lMSXcwWDdwV01idlU0TS0zXzAifSwiZXhwIjoxNjY4NzM0NTIzfQ.fcJbjxK8UpID3Fcf8lggrhSYy52UPo8lu5bpIT8FGXQ`,
+            "Authorization": "Bearer " + `${bearer}`
         }
     })
 
     const response = await fetch_country.json();
-    const country = response.map((data) => data.country_name);
-    return country;
+    const country = response.map((data) => data);
+    return { country: country, token: token };
 }
 
 // Getting Country Data Function end here
@@ -42,15 +43,20 @@ const DataByCountry = async () => {
 
 const DataByState = async () => {
     const country = await DataByCountry();
-    const fetch_state = await fetch(`https://www.universal-tutorial.com/api/states/${country[163]}`, {
+    const countryname = country.country.map((data, index) => {
+        index = data.country_phone_code
+        return data.country_name
+    })
+    const bearer = country?.token?.auth_token
+    const fetch_state = await fetch(`https://www.universal-tutorial.com/api/states/${countryname[163]}`, {
         headers: {
             "Accept": "application/json",
-            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJ6YWVlbS56YWZhckBiaW5hdGVkaWdpdGFsLmNvbSIsImFwaV90b2tlbiI6IkhLUlVRYmgtWkFKdzVnQjZUR0Q5aGhxQmZqdmZuYTJTNXhnUVJkMjVCQjU0ajhsQ1lMSXcwWDdwV01idlU0TS0zXzAifSwiZXhwIjoxNjY4NzM0NTIzfQ.fcJbjxK8UpID3Fcf8lggrhSYy52UPo8lu5bpIT8FGXQ`,
+            "Authorization": "Bearer " + `${bearer}`
         }
     });
     const resp = await fetch_state.json();
     const state = resp.map((data) => data.state_name);
-    return state;
+    return { state: state, bearer: bearer };
 }
 
 // Getting State Data Function end here
@@ -60,17 +66,28 @@ const DataByState = async () => {
 
 const DataByCity = async () => {
     const state = await DataByState();
-    const fetch_city = await fetch(`https://www.universal-tutorial.com/api/cities/${state[5]}`, {
+    const bearer = state?.bearer
+    const states = state.state.map((data) => {
+        return data;
+    })
+    const fetch_city = await fetch(`https://www.universal-tutorial.com/api/cities/${states[6]}`, {
         headers: {
             "Accept": "application/json",
-            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfZW1haWwiOiJ6YWVlbS56YWZhckBiaW5hdGVkaWdpdGFsLmNvbSIsImFwaV90b2tlbiI6IkhLUlVRYmgtWkFKdzVnQjZUR0Q5aGhxQmZqdmZuYTJTNXhnUVJkMjVCQjU0ajhsQ1lMSXcwWDdwV01idlU0TS0zXzAifSwiZXhwIjoxNjY4NzM0NTIzfQ.fcJbjxK8UpID3Fcf8lggrhSYy52UPo8lu5bpIT8FGXQ`,
+            "Authorization": "Bearer " + `${bearer}`
         }
     });
     const city = await fetch_city.json();
     const resp = city
-        .filter((data) => data.city_name !== [] ? data.city_name : console.log("No CIty in State"))
-        .map((data) => data)
-
+        .filter((data) => {
+            if (data.length < 1) {
+                const statement = "No City in this State"
+                return statement
+            }
+            else {
+                return data.city_name
+            }
+        })
+        .map((data) => data);
     return resp;
 
 }
